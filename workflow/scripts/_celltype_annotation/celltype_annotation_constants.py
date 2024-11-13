d3152f735dbbe423a7002a7a14f4d57c5428b09c
mbilous <mosbilous@gmail.com>
2024-11-13 10:39:00 +0100

    feat: init annotation rctd

---
+++ workflow/scripts/_celltype_annotation/celltype_annotation_constants.py
@@ -0,0 +1,20 @@
+"""
+String constants for reference (scRNA-seq) and query (Xenium) Seurat objects.
+"""
+
+REF_SEURAT_DEFAULT_ASSAY: str = "RNA"
+XE_SEURAT_DEFAULT_ASSAY: str = "Xenium"
+#XE_SEURAT_SPATIAL_DIM_NAME: str = "spatial" # Not used 
+
+# The following parameters may vary depending on the size of the Xenium gene panel
+# Filtering xenium cells for annotation (mb redundant, but if QC differs, its better to filter low count cells from annotation results)
+XE_MIN_UMI: int = 10 # min total UMIs
+XE_MIN_counts: int = 10 # min counts (ie., UMIs), when restricted to common genes
+
+# Filtering reference cells 
+REF_MIN_UMI: int = 10 # low, given the small number of counts in scRNA-seq data when restricted to ~300 plex xenium panel. may be usedful to be dependent on `gene_panel`, larger the panel -> more common genes between the ref and query data -> the more UMI expected to be present in the reference
+REF_MAX_UMI: int = 2000 # low, given the small number of counts in scRNA-seq data when restricted to ~300 plex xenium panel.
+
+# RCTD-specific constants
+UMI_min_sigma: int = 100 # min UMI in xenium cells to consider cell for sigma extimation (sigma of  Log-Poisson distribution) #TODO: replace with one from `config -> ... rctd -> mode -> other_options`
+CELL_MIN_INSTANCE: int = 25 # minimum number of cells required per cell type. Can be decreased for smaller references
