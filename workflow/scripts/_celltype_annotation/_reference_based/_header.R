d5bb07551f5847239ea156134fc5ed9466ec039e
mbilous <mosbilous@gmail.com>
2024-12-09 10:25:34 +0100

    fix: make sure rctd script runs correctly

--- workflow/scripts/_celltype_annotation/_reference_based/_header.R
+++ workflow/scripts/_celltype_annotation/_reference_based/_header.R
@@ -12,7 +12,7 @@ annotation_parameters <- get_referencebased_annotation_parameters(snakemake@para
 annotation_level  <- annotation_parameters[["annotation_level"]] 
 reference_type    <- annotation_parameters[["reference_type"]]
 annotation_mode   <- annotation_parameters[["annotation_mode"]] # So far, runs in single-cell mode only 
-annotation_method <- annotation_parameters[["annotation_mode"]]
+annotation_method <- annotation_parameters[["annotation_method"]]
 
 # Assays used for annotation
 ref_assay         <- snakemake@params[["ref_default_assay"]] 
@@ -23,5 +23,7 @@ REF_MIN_UMI       <- snakemake@params[["REF_MIN_UMI"]]
 REF_MAX_UMI       <- snakemake@params[["REF_MAX_UMI"]] 
 XE_MIN_UMI        <- snakemake@params[["XE_MIN_UMI"]] # mb do not provide option to specify it, but compute from Xenium object, that has been QCed already, so all cells have to be annotated?
 XE_MIN_counts     <- snakemake@params[["XE_MIN_counts"]] # same as above
+
+CELL_MIN_INSTANCE <- snakemake@params[["CELL_MIN_INSTANCE"]] # ideally, should de decreased for the breast panel, but lets keep it like this 
 
 # up to here, all the parameters are shared across all methods and diseases
