88a94b138f43a7de481c130820ad0aa21248ac70
Senbai Kang <senbaikang@gmail.com>
2025-01-24 12:17:21 +0100

    feat: add new options to `run.sh`

--- run.sh
+++ run.sh
@@ -40,6 +40,8 @@ help()
     echo "Usage: [ -m | --mode MODE ] [ -c | --core CORE ] [ -n | --dry-run ] [ -R | --forcerun RULE ] [ -U | --until RULE ] [ --dag OUTPUT ] [ --unlock ] [ -v | --verbose ] [ -h | --help ]
         -m,--mode MODE: the pipeline will be run on 'local' (default) or on 'cluster'.
         -c,--core CORE: the number of cores to be used when -m,--mode is unset or 'local' (default: 1); ignored when -m,--mode is 'cluster'.
+        -j,--jobs JOBS: the number of jobs submitted to the cluster at the same time when -m,--mode is 'cluster'. (default: 500).
+        --retries RETRIES: the number of retries for failed jobs. (default: 0).
         -n,--dry-run: dry run.
         -R,--forcerun RULE: force the re-execution or creation of the given rule or file. Repeat this option multile times for multiple rules or files.
         -U,--until RULE: runs the pipeline until it finishes the specified rule or generated the file. Repeat this option multile times for multiple rules or files.
@@ -53,8 +55,8 @@ help()
 }
 
 # Allowed command line arguments.
-SHORT_OPTS=m:,c:,n,R:,-U:,v,h
-LONG_OPTS=mode:,core:,dry-run,forcerun:,until:,dag:,unlock,verbose,help
+SHORT_OPTS=m:,c:,j:,n,R:,-U:,v,h
+LONG_OPTS=mode:,core:,jobs:,retries:,dry-run,forcerun:,until:,dag:,unlock,verbose,help
 OPTS=$(getopt -n xenium_analysis_pipeline --options $SHORT_OPTS --longoptions $LONG_OPTS -- "$@")
 eval set -- "$OPTS"
 
@@ -76,12 +78,12 @@ IFS=''
 
 # Constants.
 SINGULARITY_BIND_OPT="--bind $SINGULARITY_BIND"
-OTHER_OPT=(--rerun-triggers mtime --software-deployment-method conda apptainer --apptainer-args "--nv --no-home --cleanenv --env RUST_BACKTRACE=full $SINGULARITY_BIND_OPT" -kp)
+OTHER_OPT=(--rerun-triggers mtime --rerun-incomplete --software-deployment-method conda apptainer --apptainer-args "--nv --no-home --cleanenv --env RUST_BACKTRACE=full $SINGULARITY_BIND_OPT" -kp)
 
 # Options related to $CONDA_BIN
-if [ "$CONDA_BIN" = mamba ]; then
+if [[ "$CONDA_BIN" = mamba ]]; then
     CONDA_OPT=
-elif [ "$CONDA_BIN" = conda ]; then
+elif [[ "$CONDA_BIN" = conda ]]; then
     CONDA_OPT="--no-capture-output"
 fi
 
@@ -89,6 +91,8 @@ fi
 LOCAL=1
 EXEC_OPT=(--profile "$LOCAL_PROFILE")
 CORE_OPT=(--cores 1)
+JOBS_OPT=(--jobs 500)
+RETRIES_OPT=(--retries 0)
 DRY_RUN_OPT=
 FORCE_RUN_OPT=()
 UNTIL_OPT=()
@@ -126,6 +130,26 @@ do
                 CORE_OPT=(--cores "$2")
             else
                 echo "Invalid value for -c,--core: $2"
+                help 1
+            fi
+            shift 2
+            ;;
+
+        -j | --jobs)
+            if [[ $2 =~ ^[1-9][0-9]*$ ]]; then
+                JOBS_OPT=(--jobs "$2")
+            else
+                echo "Invalid value for -j,--jobs: $2"
+                help 1
+            fi
+            shift 2
+            ;;
+        
+        --retries)
+            if [[ $2 =~ ^[0-9]+$ ]]; then
+                RETRIES_OPT=(--retries "$2")
+            else
+                echo "Invalid value for --retries: $2"
                 help 1
             fi
             shift 2
@@ -197,7 +221,7 @@ do
 done
 
 # Set Snakemake runtime temporary directory
-if [ -n "$SNAKEMAKE_CACHE_DIR" ]; then
+if [[ -n "$SNAKEMAKE_CACHE_DIR" ]]; then
     export XDG_CACHE_HOME="$SNAKEMAKE_CACHE_DIR"
 fi
 
@@ -210,7 +234,7 @@ fi
 
 # Unlock the working directory.
 # Priority: 2 (2nd highest; other options will be ommitted)
-if [ $UNLOCK -eq 1 ]; then
+if [[ $UNLOCK -eq 1 ]]; then
     $CONDA_BIN run $CONDA_OPT -n $ENV_NAME snakemake --unlock
     exit 0
 fi
@@ -219,7 +243,7 @@ fi
 COMPLETE_CMD=(snakemake "${OTHER_OPT[@]}")
 
 # Verbose for snakemake.
-if [ $VERBOSE -eq 1 ]; then
+if [[ $VERBOSE -eq 1 ]]; then
     COMPLETE_CMD=("${COMPLETE_CMD[@]}" "--verbose")
 fi
 
@@ -227,16 +251,23 @@ fi
 COMPLETE_CMD=("${COMPLETE_CMD[@]}" "${EXEC_OPT[@]}")
 
 # Postprocess after parsing command line arguments.
-if [ $LOCAL -eq 0 ]; then
+if [[ $LOCAL -eq 0 ]]; then
+    # On cluster.
     CORE_OPT=()
+
+    # Number of jobs to be submitted to the cluster.
+    COMPLETE_CMD=("${COMPLETE_CMD[@]}" "${JOBS_OPT[@]}")
 else
+    # On local.
+    JOBS_OPT=()
+
     # Number of local cores to be used for snakemake.
     COMPLETE_CMD=("${COMPLETE_CMD[@]}" "${CORE_OPT[@]}")
 fi
 
 # Dry run mode.
 # Priority: 3
-if [ -n "$DRY_RUN_OPT" ]; then
+if [[ -n "$DRY_RUN_OPT" ]]; then
     COMPLETE_CMD=("${COMPLETE_CMD[@]}" "$DRY_RUN_OPT")
 fi
 
@@ -252,7 +283,12 @@ if [[ -n "${UNTIL_OPT[*]}" ]]; then
     COMPLETE_CMD=("${COMPLETE_CMD[@]}" --until "${UNTIL_OPT[@]}")
 fi
 
-if [ $VERBOSE -eq 1 ]; then
+# Retries for failed jobs (not for dry-run mode).
+if [[ -z "$DRY_RUN_OPT" ]]; then
+    COMPLETE_CMD=("${COMPLETE_CMD[@]}" "${RETRIES_OPT[@]}")
+fi
+
+if [[ $VERBOSE -eq 1 ]]; then
     printf "> Command used to run snakemake:\n  >> "
     echo "${COMPLETE_CMD[@]}"
 fi
