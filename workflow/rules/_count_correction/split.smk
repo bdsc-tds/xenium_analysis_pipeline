6ab0a16240d28910d3117bc1eee3f2fb6cd15e32
mbilous <mosbilous@gmail.com>
2025-03-24 11:53:56 +0100

    feat: add score based and spotclass based purification with swapping

--- workflow/rules/_count_correction/split.smk
+++ workflow/rules/_count_correction/split.smk
@@ -3,5 +3,5 @@
 ######################################
 
 include: "_split/split_fully_purified.smk"
-#include: "_purctd/purctd_spot_class_balanced.smk" # will be updated 
-#include: "_purctd/purctd_score_balanced.smk"  # will be updated 
+include: "_split/split_spot_class_balanced.smk" 
+include: "_split/split_score_balanced.smk"  
--- workflow/scripts/_count_correction/split_spot_class_balanced.R
+++ workflow/scripts/_count_correction/split_spot_class_balanced.R
@@ -13,35 +13,32 @@ snakemake@source("../../scripts/utils/readwrite.R")
 
 xe <- readRDS(snakemake@input[["xe"]])
 rctd <- readRDS(snakemake@input[["post_processed_rctd"]])
-sp_neigh_df <- read_parquet(snakemake@input[["spatial_neighborhood_scores"]])
 purified_counts <- Read10X_h5(snakemake@input[["fully_purified_counts"]])
 purified_counts_metadata <- read_parquet(snakemake@input[["fully_purified_counts_metadata"]])
 
 ###### Balancing purification level ######
 
 # Create purified Seurat
-xe_purified <- CreateSeuratObject(counts = purified_counts, meta.data = purified_counts_metadata)
+xe_purified <- CreateSeuratObject(counts = purified_counts, assay = "Xenium", meta.data = purified_counts_metadata)
 rm("purified_counts")
 
-### Run `score_based` purification (ie, purify doublets singlets that have the spatial neighborhood score larges than the threshold)
+### Run `spot_class_balanced` purification (ie, purify doublets and do not touch singlets)
 ### Done by balancing raw and purified datasets
 xe <- AddMetaData(xe, rctd@results$results_df)
-xe <- AddMetaData(xe, sp_neigh_df)
-
-xe_balanced_score <- balance_raw_and_purified_data_by_score(
+xe_balanced_spot_class <- balance_raw_and_purified_data_by_spot_class(
   xe_raw = xe,
   xe_purified = xe_purified,
-  threshold = snakemake@params[["score_threshold"]],
-  score_name = snakemake@params[["score_name"]]
+  spot_class_key = "spot_class",
+  DO_swap_lables = TRUE
 )
 
-# Output score-based purified counts
+# Output spot-class-based purified counts
 write10xCounts(
   path = snakemake@output[["corrected_counts"]], 
-  x = GetAssayData(xe_balanced_score, assay = "RNA", layer = "counts")
+  x = GetAssayData(xe_balanced_spot_class, assay = "Xenium", layer = "counts")
 )
 
 write_parquet(
-  xe_balanced_score@meta.data %>% select(all_of(colnames(purified_counts_metadata))), 
+  xe_balanced_spot_class@meta.data %>% select(all_of(colnames(purified_counts_metadata))), 
   snakemake@output[["corrected_counts_metadata"]]
 )
