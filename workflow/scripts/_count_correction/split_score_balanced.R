26168ec59a5f9d0286825a9386b4958416da12da
mbilous <mosbilous@gmail.com>
2025-04-03 11:49:56 +0200

    fix: loading large parq files

--- workflow/scripts/_count_correction/split_score_balanced.R
+++ workflow/scripts/_count_correction/split_score_balanced.R
@@ -10,16 +10,22 @@ library(SPLIT)
 
 snakemake@source("../../scripts/utils/readwrite.R")
 
-
+message("Reading data...")
 xe <- readRDS(snakemake@input[["xe"]])
 rctd <- readRDS(snakemake@input[["post_processed_rctd"]])
 sp_neigh_df <- read_parquet(snakemake@input[["spatial_neighborhood_scores"]])
 tr_neigh_df <- read_parquet(snakemake@input[["transcriptomic_neighborhood_scores"]])
 purified_counts <- Read10X_h5(snakemake@input[["fully_purified_counts"]])
-purified_counts_metadata <- read_parquet(snakemake@input[["fully_purified_counts_metadata"]])
 
+md <- open_dataset(snakemake@input[["fully_purified_counts_metadata"]], thrift_string_size_limit = 1000000000) # force reading of large files
+purified_counts_metadata <- md %>%
+  collect() %>%
+  mutate(across(everything(), unname))
+
+message("Done reading data.")
 ###### Balancing purification level ######
 
+message("Creating seurat objects...")
 # Create purified Seurat
 xe_purified <- CreateSeuratObject(counts = purified_counts, assay = "Xenium", meta.data = purified_counts_metadata)
 rm("purified_counts")
@@ -29,6 +35,9 @@ rm("purified_counts")
 xe <- AddMetaData(xe, rctd@results$results_df)
 xe <- AddMetaData(xe, sp_neigh_df)
 xe <- AddMetaData(xe, tr_neigh_df)
+message("Done creating seurat objects.")
+
+message("Balancing by score...")
 
 xe_balanced_score <- balance_raw_and_purified_data_by_score(
   xe_raw = xe,
@@ -40,6 +49,10 @@ xe_balanced_score <- balance_raw_and_purified_data_by_score(
   DO_swap_lables = TRUE # should be param, but can wait 
 )
 
+message("Done balancing by score.")
+
+message("Writing data...")
+
 # Output score-based purified counts
 write10xCounts(
   path = snakemake@output[["corrected_counts"]], 
@@ -50,3 +63,4 @@ write_parquet(
   xe_balanced_score@meta.data %>% select(all_of(c(colnames(purified_counts_metadata), "swap"))), 
   snakemake@output[["corrected_counts_metadata"]]
 )
+message("Done writing data...")
